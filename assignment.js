41f5bf84959b590eeae5c4d6ad5db83d2fab9ab4
MD MOZAMMEL HOSSAIN FAZLE RABBI <mozammelhossain.eee.buet@gmail.com>
2025-03-21 15:43:55 +0600

    Third problem solved

--- assignment.js
+++ assignment.js
@@ -12,26 +12,48 @@ function validEmail(email) {
     if (typeof email !== 'string') {
         return "Invalid";
     }
-    if(email[0] === '@' || email[0] === '.' || email[0] === '-' || email[0] === '_' || email[0] === '+')
-    {
+    if (email[0] === '@' || email[0] === '.' || email[0] === '-' || email[0] === '_' || email[0] === '+') {
         return false;
     }
-    else if(email.includes('@') === false)
-    {
+    else if (email.includes('@') === false) {
         return false;
     }
-    else if(email.includes(' ') === true)
-    {
+    else if (email.includes(' ') === true) {
         return false;
     }
-    else if(email.slice(-4) !== '.com')
-    {
+    else if (email.slice(-4) !== '.com') {
         return false;
     }
-    else
-    {
+    else {
         return true;
     }
 }
 
-console.log(validEmail("+Mewo@cat.com"));
+function electionResult(votes) {
+    if (Array.isArray(votes) === false) {
+        return "Invalid";
+    }
+    let mango = 0;
+    for (const mangoVotes of votes) {
+        if (mangoVotes === "mango") {
+            mango++;
+        }
+    }
+    let banana = 0;
+    for (const bananaVotes of votes) {
+        if (bananaVotes === "banana") {
+            banana++;
+        }
+    }
+    if (mango > banana) {
+        return "Mango";
+    }
+    else if (banana > mango) {
+        return "Banana";
+    }
+    else {
+        return "Draw";
+    }
+}
+
+console.log(electionResult(["mango", "BananA", "na vote", "na vote"]));
