e9d81d2124e573d9a32097db47942f267e943abb
MD MOZAMMEL HOSSAIN FAZLE RABBI <mozammelhossain.eee.buet@gmail.com>
2025-03-21 15:28:53 +0600

    Second problem solved

--- assignment.js
+++ assignment.js
@@ -1,12 +1,37 @@
 // Assignment-4-warriors
 
 function cashOut(money) {
-    if(typeof money !== 'number' || money <0)
-    {
+    if (typeof money !== 'number' || money < 0) {
         return "Invalid";
     }
     const charge = money * (1.75 / 100);
     return charge;
 }
 
-console.log(cashOut(0));
+function validEmail(email) {
+    if (typeof email !== 'string') {
+        return "Invalid";
+    }
+    if(email[0] === '@' || email[0] === '.' || email[0] === '-' || email[0] === '_' || email[0] === '+')
+    {
+        return false;
+    }
+    else if(email.includes('@') === false)
+    {
+        return false;
+    }
+    else if(email.includes(' ') === true)
+    {
+        return false;
+    }
+    else if(email.slice(-4) !== '.com')
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+console.log(validEmail("+Mewo@cat.com"));
