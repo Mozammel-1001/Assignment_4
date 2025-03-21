86cede36c594673167af6bb36f71f48d12736125
MD MOZAMMEL HOSSAIN FAZLE RABBI <mozammelhossain.eee.buet@gmail.com>
2025-03-21 15:56:15 +0600

    Fourth problem solved

--- assignment.js
+++ assignment.js
@@ -56,4 +56,17 @@ function electionResult(votes) {
     }
 }
 
-console.log(electionResult(["mango", "BananA", "na vote", "na vote"]));
+function isBestFriend(f1, f2) {
+    if (typeof f1 !== 'object' || typeof f2 !== 'object') {
+        return "Invalid";
+    }
+    if (f1.bestFriend === f2.roll && f2.bestFriend === f1.roll) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(isBestFriend({ name: "hashem", roll: 1, bestFriend: 1 },
+    { name: "kashem", roll: 1, bestFriend: 1 }));
